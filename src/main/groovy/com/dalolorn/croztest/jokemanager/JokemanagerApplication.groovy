ca76755ae454985ffb64ec918f0811192ded299d
Dalo Lorn <nevenp.isi@gmail.com>
2020-03-24 14:57:19 +0100

    Actually, I think I'm gonna see if Groovy's any good while I'm at this.

---
+++ src/main/groovy/com/dalolorn/croztest/jokemanager/JokemanagerApplication.groovy
@@ -0,0 +1,13 @@
+package com.dalolorn.croztest.jokemanager
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JokemanagerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(JokemanagerApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/dalolorn/croztest/jokemanager/JokemanagerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.dalolorn.croztest.jokemanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JokemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
