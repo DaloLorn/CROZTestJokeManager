b044f8becde5991c6418f14374f524b9ad3053c0
Dalo Lorn <nevenp.isi@gmail.com>
2020-03-27 18:05:13 +0100

    While I'm cleaning up already... removed a surplus import.

--- src/main/groovy/com/dalolorn/croztest/jokemanager/Joke.groovy
+++ src/main/groovy/com/dalolorn/croztest/jokemanager/Joke.groovy
@@ -1,8 +1,6 @@
 package com.dalolorn.croztest.jokemanager
 
-
 import javax.persistence.Entity
-import javax.persistence.ForeignKey
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
